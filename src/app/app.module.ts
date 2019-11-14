09a0c3d5c5b90a856dad1195eb685699a25c52df
Samir Founou <samir.founou@avizzeo.com>
2019-11-14 12:11:36 +0100

    MAJ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
  * modules demandés pour le projet :
  */
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { FormAddComponent } from './form-add/form-add.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    FormAddComponent
   ],
   imports: [
     BrowserModule,
